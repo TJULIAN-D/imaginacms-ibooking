9fa935d86cf3fa72a9fa64939f4c8d93e3bfe306
msolanorenuity <msolano@contractor.renuityhome.com>
2025-03-24 09:30:25 -0500

    added tenant config

--- Config/config.php
+++ Config/config.php
@@ -1,57 +1,66 @@
 <?php
 
 return [
-    'name' => 'Ibooking',
+  'name' => 'Ibooking',
 
-    //Media Fillables
-    'mediaFillable' => [
-        'category' => [
-            'mainimage' => 'single',
-        ],
-        'service' => [
-            'mainimage' => 'single',
-        ],
-        'resource' => [
-            'mainimage' => 'single',
-        ],
+  //Media Fillables
+  'mediaFillable' => [
+    'category' => [
+      'mainimage' => 'single',
     ],
-
-    /*
-  * Format to hour - strtotime method
-  * Used: Email
-  */
-    'hourFormat' => 'd-m-Y H:i A',
-
-    /*
-  *
-  * Config to Activities in Igamification Module
-  */
-    'activities' => [
-        [
-            'system_name' => 'availability-organize',
-            'title' => 'ibooking::activities.availability-organize.title',
-            'status' => 1,
-            'url' => 'ipanel/#/booking/resource/user/',
-        ],
-        [
-            'system_name' => 'availability-reservations',
-            'title' => 'ibooking::activities.availability-reservations.title',
-            'status' => 1,
-            'url' => 'ipanel/#/booking/reservations/index',
-        ],
-        [
-            'system_name' => 'availability-new-reservation',
-            'title' => 'ibooking::activities.availability-new-reservation.title',
-            'status' => 1,
-            'url' => 'ipanel/#/booking/reservations/create',
-        ],
+    'service' => [
+      'mainimage' => 'single',
     ],
-
-    /*Translate keys of each entity. Based on the permission string*/
-    'documentation' => [
-        'categories' => 'ibooking::cms.documentation.categories',
-        'services' => 'ibooking::cms.documentation.services',
-        'resources' => 'ibooking::cms.documentation.resources',
-        'reservations' => 'ibooking::cms.documentation.reservations',
+    'resource' => [
+      'mainimage' => 'single',
     ],
+  ],
+
+  /*
+* Format to hour - strtotime method
+* Used: Email
+*/
+  'hourFormat' => 'd-m-Y H:i A',
+
+  /*
+*
+* Config to Activities in Igamification Module
+*/
+  'activities' => [
+    [
+      'system_name' => 'availability-organize',
+      'title' => 'ibooking::activities.availability-organize.title',
+      'status' => 1,
+      'url' => 'ipanel/#/booking/resource/user/',
+    ],
+    [
+      'system_name' => 'availability-reservations',
+      'title' => 'ibooking::activities.availability-reservations.title',
+      'status' => 1,
+      'url' => 'ipanel/#/booking/reservations/index',
+    ],
+    [
+      'system_name' => 'availability-new-reservation',
+      'title' => 'ibooking::activities.availability-new-reservation.title',
+      'status' => 1,
+      'url' => 'ipanel/#/booking/reservations/create',
+    ],
+  ],
+
+  /*Translate keys of each entity. Based on the permission string*/
+  'documentation' => [
+    'categories' => 'ibooking::cms.documentation.categories',
+    'services' => 'ibooking::cms.documentation.services',
+    'resources' => 'ibooking::cms.documentation.resources',
+    'reservations' => 'ibooking::cms.documentation.reservations',
+  ],
+  //Tenant
+  'tenant' => [
+    'dbPrefix' => 'ibooking',
+    'client' => [
+      'icon' => 'fal fa-calendar-check',
+      'title' => '(pt) Get Reservations',
+      'description' => '(pt) Get Reservations Online',
+    ]
+  ]
 ];
